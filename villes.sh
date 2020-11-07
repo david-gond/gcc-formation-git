14bd074d1008bb9b553d76b8ae208139b3c4ddb8
David Gond <david.gond.coding@gmail.com>
2020-11-07 22:15:45 +0100

    fix: Fixed typo in villes.sh

--- villes.sh
+++ villes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O villes.csv "https://datanova.legroupe.lapooooste.fr/explore/dataset/laposte_hexasmal/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true"
+wget -O villes.csv "https://datanova.legroupe.laposte.fr/explore/dataset/laposte_hexasmal/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true"
 
 echo "Tri en cours…"
 <villes.csv grep -e "^33" >gironde.csv
